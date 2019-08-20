41266a74b31e6dabc6d68aecd12e3507e8e7dbe5
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-20 21:10:22 +0200

    init

--- src/Greatest_Common_Factor.java
+++ src/Greatest_Common_Factor.java
@@ -1,4 +1,22 @@
-public class Greatest_Common_Factor {
+import java.util.Scanner;
 
-    ajshg
+public class Greatest_Common_Factor {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n1 = input.nextInt();
+        System.out.print("Enter another positive integer: ");
+        int n2 = input.nextInt();
+
+        int divisor;
+        if (n1>n2) {
+            divisor = n2;
+        }else divisor = n1;
+
+        while (n1 % divisor !=0 || n2 % divisor !=0){
+            divisor--;
+        }
+
+        System.out.println(divisor);
+    }
 }
