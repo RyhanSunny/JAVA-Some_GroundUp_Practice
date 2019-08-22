0d3cc91ec1617fa5d7d54d907952c4c62794ede6
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-22 22:47:22 +0200

    student class

---
+++ src/Project__Students_and_Grades/Students.java
@@ -0,0 +1,21 @@
+package Project__Students_and_Grades;
+
+public class Students {
+    private String name;
+    private double grade;
+
+    //constructor
+    public Students(String name, double grade){
+        this.name = name;
+        this.grade = grade;
+    }
+    //getters
+    public String getName(){
+        return name;
+    }
+    public double getGrade(){
+        return grade;
+    }
+    //setters
+
+}
