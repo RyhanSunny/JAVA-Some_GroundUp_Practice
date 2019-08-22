70b1ea92954f9f1ede6a75bb41b5aeb6e74f1fc4
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-22 23:12:29 +0200

    Flowpane and multiple buttons :)

---
+++ src/Project__JavaFX/FlowPane_and_Multiple_Buttons.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class FlowPane_and_Multiple_Buttons extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage){
+        Button[] btns = new Button[5];
+        for(int i=0; i <btns.length; i++){
+            btns[i] = new Button("Button "+(i+1));
+        }
+
+        FlowPane fplane = new FlowPane();
+        fplane.setAlignment(Pos.CENTER);
+        fplane.setPadding(new Insets(10));
+        fplane.setHgap(5);
+        fplane.setVgap(10);
+
+        fplane.getChildren().addAll(btns);
+
+        primaryStage.setScene(new Scene(fplane, 400, 300));
+        primaryStage.setTitle("A COOL WINDOW WITH COOL BUTTONS");
+        primaryStage.show();
+    }
+}
