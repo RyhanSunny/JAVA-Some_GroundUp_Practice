c49650523b36fbc5a49a21afd9747fb4cd44b4da
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-23 21:22:34 +0200

    main method. run this

---
+++ src/Project__Employee_Report/Main.java
@@ -0,0 +1,26 @@
+package Project__Employee_Report;
+
+//MAIN METHOD
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Ryhan Sunny
+ */
+public class Main {
+    public static void main(String[] args) {
+        //1- DECLARE AND INSTANTIATE M251
+        Employee_Report M251 = new Employee_Report();
+
+        //2- INVOKE hireEmployee()
+        try {
+            M251.hireEmployee("C:\\employee.txt");
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error: File Not Found!");
+        }
+
+        //3- INVOKE printReport()
+        M251.printReport();
+    }
+}
